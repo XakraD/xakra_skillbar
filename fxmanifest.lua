2ab7bb8a4e4976afbefe1a8f5ac92e7380c8f60e
Xakra <zarkcat@gmail.com>
2022-12-13 18:00:43 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,18 +6,12 @@ ui_page "html/index.html"
 
 client_scripts {
     'client/main.lua',
-    'config.lua',
-}
-
-server_scripts {
-    'config.lua',
 }
 
 files {
     'html/index.html',
     'html/script.js',
     'html/style.css',
-    'html/VerdanaBold.ttf',
 }
 
 exports {
